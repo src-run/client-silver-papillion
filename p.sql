cd3762d05c4de082e0d1bf2a226276f3c2cd8444
Rob Frawley 2nd <rmf@src.run>
2016-07-25 15:55:58 -0400

    lots more crud

---
+++ p.sql
@@ -0,0 +1,10 @@
+INSERT INTO `product` VALUES 
+	(2,NULL,'2016-07-25 07:32:12','2016-07-25 07:32:12','Bicycle',NULL,NULL,1,1,'313899742-bicycle.jpg',49.99),
+	(3,NULL,'2016-07-25 07:34:03','2016-07-25 07:34:03','Captain with Binoculars',NULL,NULL,1,1,'313899867-aptain-with-binoculars.jpg',49.99),
+	(4,NULL,'2016-07-25 07:34:21','2016-07-25 07:34:21','Captain Silver',NULL,NULL,1,1,'313899877-captain-silver.jpg',49.99),
+	(5,NULL,'2016-07-25 07:34:41','2016-07-25 07:34:41','Captain Painted',NULL,NULL,1,1,'313899891-captain-painted.jpg',49.99),
+	(6,NULL,'2016-07-25 07:34:52','2016-07-25 07:35:44','Bartender',NULL,NULL,1,0,'313899903-bartender.jpg',49.99),
+	(7,NULL,'2016-07-25 07:35:16','2016-07-25 07:35:38','Attorney',NULL,NULL,1,0,'314104271-attorney.jpg',49.99),
+	(8,NULL,'2016-07-25 07:35:27','2016-07-25 07:35:27','Angel',NULL,NULL,1,0,'314104296-angel.jpg',49.99),
+	(9,NULL,'2016-07-25 07:36:04','2016-07-25 07:36:04','Bartender with Stools',NULL,NULL,1,0,'320225582-artender-with-stools.jpg',49.99);
+
